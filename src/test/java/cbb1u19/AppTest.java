14cf0e8697e29996e5d16a8fdba67c37e5f565cc
BBianca1 <boteabia29@gmail.com>
2021-10-17 19:59:23 +0100

    cv cw2 init

---
+++ src/test/java/cbb1u19/AppTest.java
@@ -0,0 +1,18 @@
+package cbb1u19;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigourous Test :-)
+     */
+	@Test
+    public void testApp() {
+        assertTrue( true );
+    }
+}
